a14ec57a42fea986b9b62c75a4a7f5f754f93e64
Daniel Szydlowski <dszydlowski@wisc.edu>
2022-11-09 11:00:32 -0600

    simulating data

--- R.R
+++ R.R
@@ -2,3 +2,14 @@
 #Tyler Butts, David Ortiz, and Danny Szydlowski
 
 #BLANK CANVAS
+
+# simulate some data
+
+x = rnorm(mean = 15, sd = 3, n = 100)
+beta_0 = 5
+beta_1 = 0.8
+
+y = beta_0 + beta_1*x + rnorm(mean = 0, sd = 2, n = 100)
+
+plot(y~x)
+
